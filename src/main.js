d04ab6cb086039ead534d267d019c48ec3567d3e
Saif <ro1c@yahoo.com>
2025-09-27 17:36:21 +0300

    feat: irrigation-systems-page

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import { initializeRouter } from "./router.js";
 import App from "./app.js";
 import HomePage from "./pages/home-page";
 import AgricultureTypesPage from "./pages/agriculture-types-page";
+import IrrigationSystemsPage from "./pages/irrigation-systems-page.js";
 import CropsAndPlantsPage from "./pages/crops-and-plants-page";
 
 // A map of routes to their corresponding page components
 const routes = {
     "/": HomePage,
     "/agriculture-types": AgricultureTypesPage,
+    "/irrigation-systems": IrrigationSystemsPage,
     "/crops-and-plants": CropsAndPlantsPage,
 };
 
